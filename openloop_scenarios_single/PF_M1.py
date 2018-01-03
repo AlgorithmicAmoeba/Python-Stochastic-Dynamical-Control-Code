37f772cfa21a500eae2d738ea2176b46a7904a20
Darren Roos <29543948+darren-roos@users.noreply.github.com>
2018-01-03 12:47:46 +0200

    Corrected measurement noise

--- openloop_scenarios_single/PF_M1.py
+++ openloop_scenarios_single/PF_M1.py
@@ -4,6 +4,7 @@ import numpy
 import src.PF as PF
 import scipy.stats
 import src.Results as Results
+import matplotlib.pyplot as plt
 
 tend = 50
 params = openloop.params.Params(tend)
@@ -27,7 +28,7 @@ prior_dist = scipy.stats.multivariate_normal(mean=init_state, cov=params.init_st
 particles = PF.init_pf(prior_dist, nP, 2)  # initialise the particles
 
 state_noise_dist = scipy.stats.multivariate_normal(cov=params.Q)  # state distribution
-meas_noise_dist = scipy.stats.multivariate_normal(params.R1)  # measurement distribution
+meas_noise_dist = scipy.stats.multivariate_normal(cov=params.R1)  # measurement distribution
 
 # Time step 1
 params.xs[:, 0] = init_state
@@ -36,10 +37,10 @@ PF.init_filter(particles, params.ys1[0], meas_noise_dist, cstr_pf)
 params.pfmeans[:, 0], params.pfcovars[:, :, 0] = PF.get_stats(particles)
 # Loop through the rest of time
 for t in range(1, params.N):
-    params.xs[:, t] = params.cstr_model.run_reactor(params.xs[:, t-1], params.us[t-1], params.h, )
+    params.xs[:, t] = params.cstr_model.run_reactor(params.xs[:, t-1], params.us[t-1], params.h)
     params.xs[:, t] += state_noise_dist.rvs()  # actual plant
     params.ys1[t] = params.C1 @ params.xs[:, t] + meas_noise_dist.rvs()  # measured from actual plant
-    PF.pf_filter(particles, params.us[t-1], params.ys1[t], state_noise_dist, meas_noise_dist, cstr_pf)
+    particles = PF.pf_filter(particles, params.us[t-1], params.ys1[t], state_noise_dist, meas_noise_dist, cstr_pf)
     params.pfmeans[:, t], params.pfcovars[:, :, t] = PF.get_stats(particles)
 
 
@@ -49,3 +50,5 @@ Results.plot_ellipses1(params.ts, params.xs, params.pfmeans, params.pfcovars, "u
 Results.plot_tracking(params.ts, params.xs, params.ys1, params.pfmeans, params.us, 1)
 
 Results.calc_error(params.xs, params.pfmeans)
+
+plt.show()
