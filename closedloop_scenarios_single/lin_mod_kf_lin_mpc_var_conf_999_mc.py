d785c515c274e40208c287ac4303a09df1c1c7c4
Darren Roos <29543948+darren-roos@users.noreply.github.com>
2018-01-16 09:23:08 +0200

    Changes to use keyword parameters

--- closedloop_scenarios_single/lin_mod_kf_lin_mpc_var_conf_999_mc.py
+++ closedloop_scenarios_single/lin_mod_kf_lin_mpc_var_conf_999_mc.py
@@ -1,6 +1,6 @@
 # Linear Plant controlled with a linear MPC using a KF to estimate the state.
 # Stochastic constraints.
 
-import closedloop_scenarios_single.lin_mod_kf_lin_mpc_var_conf
+import closedloop_scenarios_single.mod_kf_lin_mpc_var_conf
 
-closedloop_scenarios_single.lin_mod_kf_lin_mpc_var_conf.main(999, 2)
+closedloop_scenarios_single.mod_kf_lin_mpc_var_conf.main(999, mcN=2, linear=True)
