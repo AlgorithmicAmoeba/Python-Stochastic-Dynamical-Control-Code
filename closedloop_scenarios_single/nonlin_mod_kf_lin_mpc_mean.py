c62ea78aeed49b2f9d961d78c228dba6dd16deff
Darren Roos <29543948+darren-roos@users.noreply.github.com>
2018-01-17 09:19:39 +0200

    Added nonlinear mean closed loop scenarios

---
+++ closedloop_scenarios_single/nonlin_mod_kf_lin_mpc_mean.py
@@ -0,0 +1,6 @@
+# Nonlinear plant controlled with a linear MPC using a KF to estimate the state.
+# Conventional deterministic constraints.
+
+import closedloop_scenarios_single.mod_kf_lin_mpc_mean
+
+closedloop_scenarios_single.mod_kf_lin_mpc_mean.main(linear=False)
